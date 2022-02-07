306246a52478726ffb336a8acc594da2ba91b81b
Kåre Hampf <khampf@users.sourceforge.net>
2022-02-07 03:41:32 +0200

    Docker using openjdk

--- src/main/java/com/example/mortageplan/MortagePlanApplication.java
+++ src/main/java/com/example/mortageplan/MortagePlanApplication.java
@@ -5,32 +5,53 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 
-// @ComponentScan({"com.tutorialspoint.controller","com.tutorialspoint.service"})
-// @EntityScan("com.tutorialspoint.entity")
-@EnableJpaRepositories // ("com.tutorialspoint.repository")
+@EnableJpaRepositories
 @SpringBootApplication
-public class MortagePlanApplication {
-	private static final Logger logger = LoggerFactory.getLogger(MortagePlanApplication.class);
+public class MortagePlanApplication extends SpringBootServletInitializer {
+    private static final Logger logger = LoggerFactory.getLogger(MortagePlanApplication.class);
 
-	public static void main(String[] args) throws FileNotFoundException {
-		if (args.length > 0) {
-			String fileName = args[0];
-			File inputFile = new File(fileName);
-			if (!inputFile.exists()) {
-				logger.error("File " + fileName + " does not exist!");
-			} else {
-				logger.info("Loading " + fileName + " in console session.");
-				CSV csv = new CSV(inputFile);
-				Prospects prospects = new Prospects(csv);
-				System.out.println(prospects);
-			}
-		} else {
-			SpringApplication.run(MortagePlanApplication.class, args);
-		}
-	}
+    /**
+     * Used when running as a jar
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length > 0) {
+            String fileName = args[0];
+            File inputFile = new File(fileName);
+            if (!inputFile.exists()) {
+                logger.error("File " + fileName + " does not exist!");
+            } else {
+                logger.info("Loading " + fileName + " in console session.");
+                CSV csv = new CSV(inputFile);
+                Prospects prospects = new Prospects(csv);
+                System.out.println(prospects);
+            }
+        } else {
+            SpringApplication.run(MortagePlanApplication.class, args);
+        }
+    }
+
+    /**
+     * Used when running as a WAR within a web container
+     *
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MortagePlanApplication.class);
+    }
 }
+
+// TODO clear button
+// TODO manual input form?
+// TODO cleanup
+// TODO README file
--- src/main/java/com/example/mortageplan/ServletInitializer.java
+++ src/main/java/com/example/mortageplan/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.mortageplan;
+/* package com.example.mortageplan;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -10,4 +10,4 @@ public class ServletInitializer extends SpringBootServletInitializer {
 		return application.sources(MortagePlanApplication.class);
 	}
 
-}
+}*/
